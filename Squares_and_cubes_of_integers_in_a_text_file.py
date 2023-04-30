fcc7fb7e829713a8fe03f2c9f22cc8d3e845b0ee
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-30 17:35:30 +0800

    read integers.txt and create squares.txt and cubes.txt working

--- Squares_and_cubes_of_integers_in_a_text_file.py
+++ Squares_and_cubes_of_integers_in_a_text_file.py
@@ -1,6 +1,8 @@
 # Squares_and_cubes_of_integers_in_a_text_file_Marquez Francis Ivan B._BSCpE 1-5
 
 # Read integers.txt and create squares.txt and cubes.txt
+with  open("integers.txt", "r") as my_file1, open ("squares.txt", "a") as my_file2, open ("cubes.txt", "a") as my_file3:
+    pass
 # Squaring and cubing loop
 # Squaring loop
 # Append to squares.txt
