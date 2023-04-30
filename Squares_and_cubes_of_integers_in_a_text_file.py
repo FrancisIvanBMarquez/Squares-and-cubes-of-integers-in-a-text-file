72dc749fa082648e1b43e569f0765a04d6c816a5
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-30 17:33:06 +0800

    Pseudocode

--- Squares_and_cubes_of_integers_in_a_text_file.py
+++ Squares_and_cubes_of_integers_in_a_text_file.py
@@ -1,1 +1,9 @@
 # Squares_and_cubes_of_integers_in_a_text_file_Marquez Francis Ivan B._BSCpE 1-5
+
+# Read integers.txt and create squares.txt and cubes.txt
+# Squaring and cubing loop
+# Squaring loop
+# Append to squares.txt
+# Cubing loop
+# Append to cubes .txt
+
