4f62258716cbe4f5b521d3e88ac5c44d050d84cf
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-30 18:02:44 +0800

    Squaring and cubing working

--- Squares_and_cubes_of_integers_in_a_text_file.py
+++ Squares_and_cubes_of_integers_in_a_text_file.py
@@ -1,11 +1,23 @@
 # Squares_and_cubes_of_integers_in_a_text_file_Marquez Francis Ivan B._BSCpE 1-5
 
+# Squaring and cubing
+def square (integer):
+    return (integer*integer)
+
+def cube (integer):
+    return (integer*integer*integer)
 # Read integers.txt and create squares.txt and cubes.txt
 with  open("integers.txt", "r") as my_file1, open ("squares.txt", "a") as my_file2, open ("cubes.txt", "a") as my_file3:
     pass
-# Squaring and cubing loop
-# Squaring loop
+    
+    # Squaring loop
+    
+
 # Append to squares.txt
 # Cubing loop
 # Append to cubes .txt
+# test square and cube
+x = 2
+square (x)
+print (x)
 
